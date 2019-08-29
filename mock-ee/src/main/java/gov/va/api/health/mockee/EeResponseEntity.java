8588b125d9e1820ed64e646a5b48d291962fa8bc
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-08-29 12:13:48 -0400

    UPDATERATOR

--- mock-ee/src/main/java/gov/va/api/health/mockee/EeResponseEntity.java
+++ mock-ee/src/main/java/gov/va/api/health/mockee/EeResponseEntity.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Table(name = "synthea.ee_summaries")
+@Table(name = "ee_summaries", schema = "synthea")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class EeResponseEntity {
